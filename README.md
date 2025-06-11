8cf5b4ea71398fa4ac847bf01792fc05e9c32c41
quantumCode <quantumcode22@gmail.com>
2025-06-11 21:43:11 +0530

    setup a threejs scene from a gltf file

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ just a humble dice to (potentially) get started with truely random number genrat
 ### TODO:
 - [x] Random number generation func
 - [x] handle states of gening new numbers
+- [x] setup a three.js scene with a cube exported from blender
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import Dice from "./components/Dice";
 import { useDiceRoll } from "./hooks/useDiceRoll";
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, OrbitControls } from "@react-three/drei";
 
 function App() {
   const {
@@ -9,10 +12,24 @@ function App() {
     biasAmount,
     setBaisAmount,
     randomNumGen,
-  } = useDiceRoll(6)
+  } = useDiceRoll(6);
+
+  const {nodes, materials} = useGLTF('/dice.gltf')
+
+  console.log(nodes)
+  console.log(materials)
 
   return (
     <>
+      <Canvas className="w-[20rem] h-[20rem] border-2 border-red-400">
+        <ambientLight intensity={1} />
+        <directionalLight color="white" position={[0, 5, 5]} />
+        <OrbitControls />
+        <mesh geometry={nodes.dice.geometry} material={materials.dice_material} />
+
+        {/* <Dice /> */}
+      </Canvas>
+
       <h1>Random Number</h1>
       <p>{randNum}</p>
       <button onClick={() => setRandNum(randomNumGen())}>Roll the dice!</button>
